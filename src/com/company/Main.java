a0d85e35268bfec03b52817bb67aa194bbb438b8
Saltanat2020 <saabdubek@gmail.com>
2020-11-12 10:45:53 -0500

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git");
-
+        //B21 is cool!
+        //B21is excited!
 
     }
 }
